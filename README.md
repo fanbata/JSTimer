e8627706a73d9c804657d2a209cf233d08d2f7b0
Daniel Yoon <fanbata@uw.edu>
2018-05-15 18:47:50 -0700

    Adjusted structure of timer to be more flexible.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # JSTimer
-Simple Javascript timer. More like a stopwatch, I guess.
+Simple Javascript timer. Currently in-progress.
 
+Changelog:
+
+v0.2
+	- Actually functions like a timer now.
+	- Can now support multiple timers.
+	- Beginnings of mobile-first design.
+
+Old version can be seen here:
 [Demo](http://fanbata.github.io/timer/timer.html)
--- timer.js
+++ timer.js
@@ -1,121 +1,55 @@
-// Let's build a timer //
-// TODO: Make timer self-adjusting? //
-document.addEventListener('DOMContentLoaded', function() {
-	var timer = null;
+var Timer = function(limit) {
+    //initial time
+    this.initial = Date.now();
+    this.current = limit;
+    this.limit = limit;
+    this.offset = 0;
+    this.old = 0;
+    //0 = paused, 1 = active, 2 = inactive
+    this.state = 0;
+    this.id = undefined;
+};
 
-	// button actions //
-	document.getElementById('start').addEventListener('click', function() {
-		selectThisButton(this);
-		if (timer !== null) {
-			timer.start();
-		} else {
-			timer = new Timer();
-			timer.begin();
-		}
-	});
+Timer.prototype.update = function() {
+    if (this.state > 0) {
+        this.current = this.limit - (Date.now() - this.initial) + this.offset;
+        if (this.current < 0) {
+            this.current = 0;
+            this.state = -1;
+        }
+    } else if (this.state === 0) {
+        this.offset = Date.now() - this.old;
+    } else {
+        return;
+    }
+};
 
-	document.getElementById('stop').addEventListener('click', function() {
-		selectThisButton(this);
-		if (timer !== null) {
-			timer.stop();
-		} else {
-			return;
-		}
-	});
+Timer.prototype.isComplete = function() {
+    return this.state < 1;
+};
 
-	document.getElementById('clear').addEventListener('click', function() {
-		removeSelects();
-		if (timer !== null) {
-			timer.clear();
-			timer = null;
-		} else {
-			return;
-		}
-	});
+Timer.prototype.setID = function(id) {
+    this.id = id;
+};
 
-	// helper functions for button styling //
-	function selectThisButton(ele) {
-		removeSelects();
-		ele.classList += " selected";
-	}
+Timer.prototype.getID = function() {
+    return this.id;
+};
 
-	function removeSelects() {
-		const selected = document.getElementsByClassName('selected');
-		if (selected.length > 0) {
-			selected[0].className = "button";
-		}
-	}
+Timer.prototype.getCurrent = function() {
+    return this.current;
+};
 
-	// Timer object //
-	function Timer() {
-		this.interval = null;
-		this.init = Date.now();
-		this.delta = 0;
-		this.old = 0;
-		this.offset = 0;
-		this.paused = 1;
+Timer.prototype.start = function() {
+    this.state = 1;
 
-		this.hEle = document.getElementById('hours');
-		this.mEle = document.getElementById('minutes');
-		this.sEle = document.getElementById('seconds');
-	}
+    //add the offset to the limit so that
+    //we do not have to keep track of an ever-growing offset.
+    this.limit += this.offset;
+    this.offset = 0;
+};
 
-	Timer.prototype.begin = function() {
-		this.start();
-		this.interval = setInterval(this.update.bind(this), 200);
-	};
-
-	Timer.prototype.update = function() {
-		if (this.paused) return;
-
-		//get how much time has passed
-		const now = Date.now();
-		this.delta = now - this.init - this.offset;
-
-		//convert to seconds
-		const s = Math.floor((this.delta % 60000) / 1000);
-		if (s < 10) {
-			this.sEle.innerHTML = "0" + s;
-		} else {
-			this.sEle.innerHTML = s;
-		}
-
-		//convert to minutes
-		const m = Math.floor(this.delta / 60000) % 60;
-		if (m < 10) {
-			this.mEle.innerHTML = "0" + m;
-		} else {
-			this.mEle.innerHTML = m;
-		}
-
-		//convert to hours
-		const h = Math.floor(this.delta / 3600000);
-		this.hEle.innerHTML = h;
-	}
-
-	Timer.prototype.start = function () {
-		if (this.paused < 1) return;
-
-		//set how much time to offset since
-		//timer was paused.
-		if (this.interval !== null) {
-			this.offset += Date.now() - this.old;
-		}
-		this.paused = 0;
-	};
-
-	Timer.prototype.stop = function() {
-		if (this.paused > 0) return;
-		this.paused = 1;
-
-		//keep track of paused time for offset.
-		this.old = Date.now();
-	};
-
-	Timer.prototype.clear = function() {
-		clearInterval(this.interval);
-		this.hEle.innerHTML = "0";
-		this.mEle.innerHTML = "00";
-		this.sEle.innerHTML = "00";
-	};
-});
+Timer.prototype.pause = function() {
+    this.state = 0;
+    this.old = Date.now();
+};
